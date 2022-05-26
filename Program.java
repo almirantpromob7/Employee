70e0c9e55fb2bdce873100035cbe6b77516c551a
Rafael Almirante <88936758+almirantpromob7@users.noreply.github.com>
2022-05-25 22:12:54 -0300

    Arquivos adicionados via Upload

---
+++ Program.java
@@ -0,0 +1,36 @@
+package application2;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities2.Employee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Employee emp = new Employee();
+		
+		System.out.println("Name: ");
+		emp.name = sc.nextLine();
+		System.out.print("Gloss Salary: ");
+		emp.grossSalary = sc.nextDouble();
+		System.out.println("Tax: ");
+		emp.tax = sc.nextDouble();
+		
+		System.out.println();
+		System.out.println("Employee: " + emp);
+		System.out.println();
+		System.out.print("Which percentage to increase salary? ");
+		double percentage = sc.nextDouble();
+		emp.increaseSalary(percentage);
+		
+		System.out.println();
+		System.out.println("Updated data: " + emp);
+		
+		sc.close();
+	}
+
+}
